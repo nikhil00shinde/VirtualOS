3cf420d731a6ba1fa34f994bc222b72e363b0ed9
Nikhil Shinde <nikhilshinde00@outlook.com>
2022-05-20 21:10:22 +0530

    go basic syntax

---
+++ practice/linear.go
@@ -0,0 +1,24 @@
+// Linear Search
+
+package main
+
+import (
+	"fmt"
+)
+
+func linearSearch(dataList []int,key int) bool{
+   
+   for _,item := range dataList{
+		 if item == key{
+			 return true
+		 }
+	 }
+	 return false
+}
+
+
+func main(){
+    
+	var dataList []int = []int{1,2,3,453,2,34,2,8}
+   fmt.Println(linearSearch(dataList,8))
+}
